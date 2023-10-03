210f3829a1205325275cfadb2f82620e28c7e7e3
dllliu <dlliu@umich.edu>
2023-10-02 21:03:46 -0400

    add starter project files

--- starter_project/autonomy/src/localization.py
+++ starter_project/autonomy/src/localization.py
@@ -6,6 +6,7 @@ import numpy as np
 # library for interacting with ROS and TF tree
 import rospy
 import tf2_ros
+import math
 
 # ROS message types we need to use
 from sensor_msgs.msg import NavSatFix, Imu
@@ -20,6 +21,8 @@ class Localization:
     def __init__(self):
         # create subscribers for GPS and IMU data, linking them to our callback functions
         # TODO
+        rospy.Subscriber("/gps/fix", NavSatFix, self.gps_callback)
+        rospy.Subscriber("/imu/imu_only", Imu, self.imu_callback)
 
         # create a transform broadcaster for publishing to the TF tree
         self.tf_broadcaster = tf2_ros.TransformBroadcaster()
@@ -35,6 +38,7 @@ class Localization:
         that pose to the TF tree.
         """
         # TODO
+        print(msg)
 
     def imu_callback(self, msg: Imu):
         """
@@ -43,6 +47,7 @@ class Localization:
         store that value in `self.pose`, then publish that pose to the TF tree.
         """
         # TODO
+        print(msg)
 
     @staticmethod
     def spherical_to_cartesian(spherical_coord: np.ndarray, reference_coord: np.ndarray) -> np.ndarray:
@@ -57,6 +62,16 @@ class Localization:
         :returns: the approximated cartesian coordinates in meters, given as a numpy array [x, y, z]
         """
         # TODO
+        # rotate by 90 clockwise = (y,-x)
+        referenceLat = 42.293195
+        referenceLong = -83.7096706
+        crc = 6371000
+        latDist = (
+            spherical_coord[0] - referenceLat
+        ) * crc  # converts horizontal dist from degrees to meter, this becomes the x coordinate
+        longDist = (
+            (spherical_coord[1] - referenceLong) * crc * (np.cos(referenceLat * (math.PI / 180)))
+        )  # converts longitude distance to meters
 
 
 def main():
--- starter_project/autonomy/src/perception.cpp
+++ starter_project/autonomy/src/perception.cpp
@@ -1,7 +1,10 @@
 #include "perception.hpp"
+#include "mrover/StarterProjectTag.h"
+#include "ros/param.h"
 
 // ROS Headers, ros namespace
 #include <ros/init.h>
+#include <vector>
 
 int main(int argc, char** argv) {
     ros::init(argc, argv, "starter_project_perception"); // Our node name (See: http://wiki.ros.org/Nodes)
@@ -26,7 +29,7 @@ namespace mrover {
         // Create a publisher for our tag topic
         // See: http://wiki.ros.org/ROS/Tutorials/WritingPublisherSubscriber%28c%2B%2B%29
         // TODO: uncomment me!
-        // mTagPublisher = mNodeHandle.advertise<StarterProjectTag>("tag", 1);
+        mTagPublisher = mNodeHandle.advertise<StarterProjectTag>("tag", 1);
 
         mTagDetectorParams = cv::aruco::DetectorParameters::create();
         mTagDictionary = cv::aruco::getPredefinedDictionary(0);
@@ -48,21 +51,45 @@ namespace mrover {
 
     void Perception::findTagsInImage(cv::Mat const& image, std::vector<StarterProjectTag>& tags) { // NOLINT(*-convert-member-functions-to-static)
         // hint: take a look at OpenCV's documentation for the detectMarkers function
+
+
         // hint: you have mTagDictionary, mTagCorners, mTagIds, and mTagDetectorParams member variables already defined!
+
         // hint: write and use the "getCenterFromTagCorners" and "getClosenessMetricFromTagCorners" functions
 
         tags.clear(); // Clear old tags in output vector
+        cv::aruco::detectMarkers(image, mTagDictionary, mTagCorners, mTagIds, mTagDetectorParams);
 
-        // TODO: implement me!
+
+        int numTagsDetected = (int) mTagIds.size();
+        StarterProjectTag msg;
+
+        for (int i = 0; i < numTagsDetected; i++) {
+            std::pair<float, float> center = getCenterFromTagCorners(mTagCorners[i]);
+            float closeness = getClosenessMetricFromTagCorners(image, mTagCorners[i]);
+
+            msg.tagId = i;
+            msg.xTagCenterPixel = center.first;
+            msg.yTagCenterPixel = center.second;
+            msg.closenessMetric = closeness;
+            tags.push_back(msg);
+        }
     }
 
     StarterProjectTag Perception::selectTag(std::vector<StarterProjectTag> const& tags) { // NOLINT(*-convert-member-functions-to-static)
-        // TODO: implement me!
-        return {};
+        float minn = 1000.0;
+        StarterProjectTag ans = tags[0];
+        for (int i = 0; i < tags.size(); i++) {
+            if (pow(tags[i].xTagCenterPixel, 2) - pow(tags[i].yTagCenterPixel, 2 < minn)) {
+                minn = pow(tags[i].xTagCenterPixel, 2) - pow(tags[i].yTagCenterPixel, 2);
+                ans = tags[i];
+            };
+        };
+        return ans;
     }
 
     void Perception::publishTag(StarterProjectTag const& tag) {
-        // TODO: implement me!
+        mTagPublisher.publish(tag);
     }
 
     float Perception::getClosenessMetricFromTagCorners(cv::Mat const& image, std::vector<cv::Point2f> const& tagCorners) { // NOLINT(*-convert-member-functions-to-static)
@@ -70,13 +97,22 @@ namespace mrover {
         // hint: this is an approximation that will be used later by navigation to stop "close enough" to a tag.
         // hint: try not overthink, this metric does not have to be perfectly accurate, just correlated to distance away from a tag
 
-        // TODO: implement me!
-        return {};
+        float xWidth = tagCorners[1].x - tagCorners[0].x;
+        float yHeight = tagCorners[2].y - tagCorners[1].y;
+        float tag_area = xWidth * yHeight;
+
+        int camera_width = image.rows;  //width of image
+        int camera_height = image.cols; //height of image
+        int camera_area = camera_width * camera_height;
+
+        return 1 - (tag_area / (float) camera_area);
     }
 
     std::pair<float, float> Perception::getCenterFromTagCorners(std::vector<cv::Point2f> const& tagCorners) { // NOLINT(*-convert-member-functions-to-static)
         // TODO: implement me!
-        return {};
+        float width = abs(tagCorners[1].x - tagCorners[0].x);
+        float height = abs(tagCorners[2].y - tagCorners[1].y);
+        return {width / 2, height / 2};
     }
 
 } // namespace mrover
